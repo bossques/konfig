ef56c22e6fb2715858608f5ab1f5e8cb338f7c5d
qbosst <notlukas555@gmail.com>
2021-07-13 22:32:14 +0100

    change string serializers

--- core/src/main/kotlin/me/qbosst/konfig/Konfig.kt
+++ core/src/main/kotlin/me/qbosst/konfig/Konfig.kt
@@ -1,15 +1,11 @@
 package me.qbosst.konfig
 
-import kotlinx.serialization.*
-import kotlinx.serialization.builtins.MapEntrySerializer
 import kotlinx.serialization.builtins.MapSerializer
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.builtins.serializer
 import me.qbosst.konfig.engine.SerializationEngine
 import me.qbosst.konfig.util.getSerialName
 import java.io.File
 import kotlin.reflect.KProperty1
-import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.isAccessible
 
 open class Konfig<E: Any>(
@@ -39,39 +35,33 @@ open class Konfig<E: Any>(
         map = read().toMutableMap()
 
         @Suppress("UNCHECKED_CAST")
-        runCatching { validate() }.onFailure { t ->
-            if(t is ConfigPropertiesMissingException) {
-                t.properties.forEach { propName ->
-                    val prop = getProperty(propName)!! as KProperty1<Configurable<*>, *>
+        try { validate() } catch (e: ConfigPropertiesMissingException) {
+            e.properties.forEach { propName ->
+                val prop = getProperty(propName)!! as KProperty1<Configurable<*>, *>
 
-                    prop.isAccessible = true
-                    val delegate = prop.getDelegate(this) as DelegatedConfigProperty<out Any?, out Any, *, E, *>
-                    prop.isAccessible = false
-                    map[propName] = delegate.default
-                }
-
-                write()
-                return
+                prop.isAccessible = true
+                val delegate = prop.getDelegate(this) as DelegatedConfigProperty<out Any?, out Any, *, E, *>
+                prop.isAccessible = false
+                map[propName] = delegate.default
             }
-            throw t
+
+            write()
         }
     }
 
-    @OptIn(InternalSerializationApi::class)
     fun write() {
         val content = serializationEngine.engine.encodeToString(
-            MapSerializer(String::class.serializer(), serializationEngine.elementSerializer), map
+            MapSerializer(String.serializer(), serializationEngine.elementSerializer), map
         )
 
         file.writeText(content)
     }
 
-    @OptIn(InternalSerializationApi::class)
     fun read(): Map<String, E> {
         val content = file.readText()
 
         return serializationEngine.engine.decodeFromString(
-            MapSerializer(String::class.serializer(), serializationEngine.elementSerializer), content
+            MapSerializer(String.serializer(), serializationEngine.elementSerializer), content
         )
     }
 
