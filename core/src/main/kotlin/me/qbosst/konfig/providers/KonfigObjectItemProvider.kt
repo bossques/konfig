18dbb767d39d7fd73b7b29c464da66449ff6cb79
qbosst <notlukas555@gmail.com>
2021-08-02 19:43:16 +0100

    use property name for konfig objects

--- core/src/main/kotlin/me/qbosst/konfig/providers/KonfigObjectItemProvider.kt
+++ core/src/main/kotlin/me/qbosst/konfig/providers/KonfigObjectItemProvider.kt
@@ -3,10 +3,10 @@ package me.qbosst.konfig.providers
 import me.qbosst.konfig.KonfigObject
 import me.qbosst.konfig.KonfigObjectItem
 import me.qbosst.konfig.Konfigurable
+import me.qbosst.konfig.util.getSerialName
 import kotlin.properties.PropertyDelegateProvider
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
-import kotlin.reflect.jvm.jvmName
 
 class KonfigObjectItemProvider<T: KonfigObject, DELEGATE>(
     val kClass: KClass<T>,
@@ -15,7 +15,7 @@ class KonfigObjectItemProvider<T: KonfigObject, DELEGATE>(
 ): PropertyDelegateProvider<Konfigurable, DELEGATE> {
 
     override fun provideDelegate(thisRef: Konfigurable, property: KProperty<*>): DELEGATE {
-        val name = kClass.jvmName
+        val name = property.getSerialName()
 
         val item = KonfigObjectItem(name, instance)
         thisRef.items[item.name] = item
